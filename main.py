5093856aee8c174082dfd5c3f2fc6f6f56b9dfed
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-04-05 21:52:13 +0100

    display stock information

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+from locale import currency
 import yfinance as yf
+import os
+import colorama
+from colorama import Fore, Back
+colorama.init(autoreset=True)
 
+os.system("clear")
+global symbol
 symbol = input("Enter the ticker you would like a rating of: ")
+os.system("clear")
 
 ticker = yf.Ticker(symbol)
 start='2020-09-15',
 end='2020-11-15',
 
-print(ticker.info)
+global shortName, tz, sector, country
+shortName = ticker.info["shortName"]
+tz = ticker.info["exchangeTimezoneName"]
+country = ticker.info["country"]
+currencyS = ticker.info["financialCurrency"]
+
+
+def stockInfo():
+    print(f"" + Back.WHITE + Fore.BLACK + "Stock Information")
+    print(f"\nStock name: " + Fore.YELLOW + shortName)
+    print(f"Exchange Time Zone: " + Fore.YELLOW + tz)
+    print(f"Country: " + Fore.YELLOW + country)
+    print(f"Currency: " + Fore.YELLOW + currencyS)
+    cont()
+
+def cont():
+    input("\nPress [ENTER] to continue ")
+
+stockInfo()
