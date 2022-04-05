e236ca6f136ffedebbc52ac6a1cf12fd7832a7c9
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-04-05 22:21:30 +0100

    removed unneccessary variables

--- main.py
+++ main.py
@@ -6,39 +6,32 @@ import colorama
 from colorama import Fore, Back
 colorama.init(autoreset=True)
 
-os.system("clear")
-global symbol
-symbol = input("Enter the ticker you would like a rating of: ")
-os.system("clear")
-
-ticker = yf.Ticker(symbol)
-start='2020-09-15',
-end='2020-11-15',
-
-shortName = ticker.info["shortName"]
-tz = ticker.info["exchangeTimezoneName"]
-country = ticker.info["country"]
-currencyS = ticker.info["financialCurrency"]
-sector = ticker.info["sector"]
-
-
 def stockInfo():
+    os.system("clear")
     print(f"" + Back.WHITE + Fore.BLACK + " Basic Information about " + symbol + " ")
-    print(f"\nStock name: " + Fore.YELLOW + shortName)
-    print(f"Sector: " + Fore.YELLOW + sector)
-    print(f"Country: " + Fore.YELLOW + country)
-    print(f"Exchange Time Zone: " + Fore.YELLOW + tz)
-    print(f"Currency: " + Fore.YELLOW + currencyS)
+    print(f"\nStock name: " + Fore.YELLOW + ticker.info["shortName"])
+    print(f"Sector: " + Fore.YELLOW + ticker.info["sector"])
+    print(f"Country: " + Fore.YELLOW + ticker.info["country"])
+    print(f"Exchange Time Zone: " + Fore.YELLOW + ticker.info["exchangeTimezoneName"])
+    print(f"Currency: " + Fore.YELLOW + ticker.info["financialCurrency"])
     cont()
 
 def financialData():
     print(f"" + Back.WHITE + Fore.BLACK + " Financial Information about " + symbol + " \n")
     rawData = ticker.history(period="7d")
     print(rawData)
+    cont()
 
 def cont():
     input("\nPress [ENTER] to continue ")
     os.system("clear")
 
-stockInfo()
-financialData()
+def run():
+    os.system("clear")
+    global symbol, ticker
+    symbol = input("Enter the ticker you would like a rating of: ")
+    ticker = yf.Ticker(symbol)
+    stockInfo()
+    financialData()
+
+run()
