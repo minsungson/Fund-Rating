e9dc28c2438f0aa68adbcc1a1773429925b8f89d
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-04-13 12:30:31 +0100

    class=

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from curses import raw
+from locale import currency
 import yfinance as yf
 import os
 from datetime import datetime, timedelta
@@ -11,33 +11,48 @@ startDate = datetime.today()
 endDate = startDate + \
     timedelta(days=7)
 
+class stock():
+    def __init__(self, sector, country, exchangeTimezoneName, financialCurrency, ISIN, majorHolders, instututionalHolders):
+        self.sector = sector
+        self.country = country
+        self.exchangeTimezoneName = exchangeTimezoneName
+        self.financialCurrency = financialCurrency
+        self.ISIN = ISIN
+        self.major_holders = majorHolders
+        self.institutional_holders = instututionalHolders
+
+    def getStats(self):
+        return [self.sector, self.country, self.exchangeTimezoneName, self.financialCurrency, self.ISIN, self.major_holders, self.institutional_holders]
+
 def stockInfo():
     os.system("clear")
-    print(f"" + Back.WHITE + Fore.BLACK + " Basic Information about " + ticker.info["shortName"] + " \n")
-    print(f"Sector: " + Fore.YELLOW + ticker.info["sector"])
-    print(f"Country: " + Fore.YELLOW + ticker.info["country"])
-    print(f"Exchange Time Zone: " + Fore.YELLOW + ticker.info["exchangeTimezoneName"])
-    print(f"Currency: " + Fore.YELLOW + ticker.info["financialCurrency"])
+    ticker.getStats()
+    # print("" + Back.WHITE + Fore.BLACK + " Basic Information about " + ticker.info["shortName"] + " \n")
+    # print("Sector: " + Fore.YELLOW + ticker.info["sector"])
+    # print("Country: " + Fore.YELLOW + ticker.info["country"])
+    # print("Exchange Time Zone: " + Fore.YELLOW + ticker.info["exchangeTimezoneName"])
+    # print("Currency: " + Fore.YELLOW + ticker.info["financialCurrency"])
+    # print("ISIN: " + Fore.YELLOW + ticker.info["financialCurrency"])
     cont()
 
-def financialData():
-    print(f"" + Back.WHITE + Fore.BLACK + " Financial Information about " + ticker.info["shortName"] + " \n")
-    global rawData
-    # rawData = ticker.history(period="7d")
-    print(rawData[["Open"]])
-    cont()
+# def financialData():
+#     print("" + Back.WHITE + Fore.BLACK + " Financial Information about " + ticker.info["shortName"] + " \n")
+#     # global rawData
+#     # rawData = ticker.history(period="7d")
+#     # print(rawData[["Open"]])
+#     cont()
 
-def calculation():
-    df = pd.DataFrame(rawData)
-    list = df['Open'].values.tolist()
-    # sigma = rawData.sum()
-    # print("Σ open =", sigma)
-    # print("     x̄ =", sigma/7)
-    print(type(rawData))
+# # def calculation():
+# #     df = pd.DataFrame(rawData)
+# #     list = df['Open'].values.tolist()
+# #     # sigma = rawData.sum()
+# #     # print("Σ open =", sigma)
+# #     # print("     x̄ =", sigma/7)
+# #     print(type(rawData))
 
-def cont():
-    input("\nPress [ENTER] to continue ")
-    os.system("clear")
+# def cont():
+#     input("\nPress [ENTER] to continue ")
+#     os.system("clear")
 
 def run():
     os.system("clear")
@@ -45,8 +60,12 @@ def run():
     # symbol = input("Enter the ticker you would like a rating of: ")
     symbol = "GOOGL" # set static for debugging
     ticker = yf.Ticker(symbol)
-    # stockInfo()
+    ticker = stock(self.sector, self.country, self.exchangeTimezoneName, self.financialCurrency, self.ISIN, self.major_holders, self.institutional_holders)
+    stockInfo()
     # financialData()
-    calculation()
+    # calculation()
 
-# run()
+run()
+
+# ticker = yf.Ticker("GOOGL")
+# print(ticker.isin)
