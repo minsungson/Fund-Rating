e965dbf80682d910b07c4b8910667216f1c75af0
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-04-13 14:10:14 +0100

    prints all stats, otherwise prints N/A

--- main.py
+++ main.py
@@ -14,10 +14,40 @@ endDate = startDate + \
 def stockInfo():
     os.system("clear")
     print("" + Back.WHITE + Fore.BLACK + " Basic Information about " + ticker.info["shortName"] + " \n")
-    print("Sector: " + Fore.YELLOW + ticker.info["sector"])
-    print("Country: " + Fore.YELLOW + ticker.info["country"])
-    print("Exchange Time Zone: " + Fore.YELLOW + ticker.info["exchangeTimezoneName"])
-    print("Currency: " + Fore.YELLOW + ticker.info["financialCurrency"])
+    # print("Sector: " + Fore.YELLOW + ticker.info["sector"])
+    if "sector" in ticker.info:
+        print("Sector: " + Fore.YELLOW + ticker.info["sector"])
+    else:
+        print("Sector" + Fore.RED + "N/A")
+    if "country" in ticker.info:
+        print("Country: " + Fore.YELLOW + ticker.info["country"])
+    else:
+        print("Country" + Fore.RED + "N/A")
+    if "exchangeTimezoneName" in ticker.info:
+        print("Trading Time Zone: " + Fore.YELLOW + ticker.info["exchangeTimezoneName"])
+    else:
+        print("Trading Time Zone " + Fore.RED + "N/A")
+    if "financialCurrency" in ticker.info:
+        print("Trading Currency: " + Fore.YELLOW + ticker.info["financialCurrency"])
+    else:
+        print("Trading Currrency: " + Fore.RED + "/A")
+    if "isin" in ticker.info:
+        print("ISIN: " + Fore.YELLOW + ticker.info["isin"])
+    else:
+        print("ISIN: " + Fore.RED + "N/A")
+    if "major_shareholders" in ticker.info:
+        print("Major Shareholders: " + Fore.YELLOW + ticker.info["major_shareholders"])
+    else:
+        print("Major Shareholders: " + Fore.RED + "N/A")
+    if "institutional_holders" in ticker.info:
+        print("Institutional Holders:" + Fore.YELLOW + ticker.info["institutional_holders"])
+    else:
+        print("Institutional Holders: " + Fore.RED + "N/A")
+    if "dividend" in ticker.info:
+        print("Latest Dividend Payment: " + Fore.YELLOW + ticker.info["dividend"])
+    else:
+        print("Latest Dividend Payment: " + Fore.RED + "N/A")
+
     cont()
 
 def financialData():
@@ -35,9 +65,9 @@ def financialData():
 # #     # print("     x̄ =", sigma/7)
 # #     print(type(rawData))
 
-# def cont():
-#     input("\nPress [ENTER] to continue ")
-#     os.system("clear")
+def cont():
+    input("\nPress [ENTER] to continue ")
+    os.system("clear")
 
 def run():
     os.system("clear")
@@ -45,7 +75,6 @@ def run():
     # symbol = input("Enter the ticker you would like a rating of: ")
     symbol = "GOOGL" # set static for debugging
     ticker = yf.Ticker(symbol)
-    ticker = stock(self.sector, self.country, self.exchangeTimezoneName, self.financialCurrency, self.ISIN, self.major_holders, self.institutional_holders)
     stockInfo()
     # financialData()
     # calculation()
