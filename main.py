2979ded2623b611e680b3f276bb24ccad038f2d4
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-04-14 15:40:15 +0100

    colourised

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def stockInfo():
     print("" + Back.WHITE + Fore.BLACK + " Basic Info about " + ticker.info["shortName"] + " (" + ticker.info["symbol"] + ") \n")
     key = {"Type: ": "quoteType", "Sector: ": "sector", "Country: ": "country", "Trading Time Zone: ": "exchangeTimezoneShortName: ", "Trading Currency: ": "financialCurrency: ", "ISIN: ": "isin", "Current Price: ": "regularMarketPrice", "Summary: ": "longBusinessSummary"}
     for i, m in key.items():
-        print(i + str(ticker.info.get(m)))
+        if ticker.info.get(m) == None:
+            print(i + Fore.RED + str(ticker.info.get(m)))
+        else:
+            print(i + Fore.YELLOW + str(ticker.info.get(m)))
     cont()
 
 def financialData():
