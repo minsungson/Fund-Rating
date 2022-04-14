f7ae881de8af2949c949d2d09c419988f4081ed1
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-04-14 14:48:09 +0100

    for loop to iterate scrape req. data

--- list testing.py
+++ list testing.py
@@ -1,8 +1,7 @@
 import yfinance as yf
-import pandas as pd
 
 ticker = yf.Ticker("GOOGL")
 
-key = {"sector", "country", "exchangeTimezoneName", "financialCurrency", "isin", "majorHolders", "instututionalHolders"}
+key = ("sector", "country", "exchangeTimezoneName", "financialCurrency", "isin", "majorHolders", "instututionalHolders")
 for i in key:
-    print(ticker.info.get(key, "doesnt exist"))
+    print(ticker.info.get(i))
